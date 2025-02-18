67e7a6898978b536e1c02d5c5fb0c3cb1a54dfa2
Trenton Cochell <trentoncochell@gmail.com>
2025-02-17 21:27:16 -0600

    Implemented string buffer methods

--- src/main/java/edu/grinnell/csc207/texteditor/SimpleStringBuffer.java
+++ src/main/java/edu/grinnell/csc207/texteditor/SimpleStringBuffer.java
@@ -4,36 +4,80 @@ package edu.grinnell.csc207.texteditor;
  * A naive implementation of a text buffer using a <code>String</code>.
  */
 public class SimpleStringBuffer {
+    StringBuffer SimpleStringBuffer = new StringBuffer();
+    int cursor = 0;
+    
+    /**
+     * Inserts the specified character into the string buffer.
+     * @param ch The character to insert into the buffer
+     */
     public void insert(char ch) {
-        throw new UnsupportedOperationException("Unimplemented method 'delete'");
+        SimpleStringBuffer.insert(getCursorPosition(),ch);
+        cursor++;
     }
 
+    /**
+     * Deletes the value at the current cursor position, then decrements the 
+     * cursor by one; Returns null if the string buffer is empty.
+     */
     public void delete() {
-        throw new UnsupportedOperationException("Unimplemented method 'delete'");
+        if (cursor == 0)
+            return;
+        SimpleStringBuffer.delete(getCursorPosition()-1,getCursorPosition());
+        cursor--;
     }
 
+    /**
+     * Returns the current cursor position.
+     * @return the current cursor position.
+     */
     public int getCursorPosition() {
-        throw new UnsupportedOperationException("Unimplemented method 'getCursorPosition'");
+        return(cursor);
     }
 
+    /**
+     * Decrements the cursor by one; unless the cursor is at the start, then
+     * nothing happens.
+     */
     public void moveLeft() {
-        throw new UnsupportedOperationException("Unimplemented method 'moveLeft'");
+        if (cursor == 0)
+            return;
+        cursor--;
     }
 
+    /**
+     * Increments the cursor by one; unless the cursor is at the end, then
+     * nothing happens.
+     */
     public void moveRight() {
-        throw new UnsupportedOperationException("Unimplemented method 'moveRight'");
+        if (cursor == getSize())
+            return;
+        cursor++;
     }
 
+    /**
+     * Returns the size of the string buffer.
+     * @return The length (size) of the string buffer.
+     */
     public int getSize() {
-        throw new UnsupportedOperationException("Unimplemented method 'getSize'");
+        return(SimpleStringBuffer.length());
     }
 
+    /**
+     * Gets a char from a string buffer at the index, i.
+     * @param i The index to get a char from.
+     * @return The character at the index, i.
+     */
     public char getChar(int i) {
-        throw new UnsupportedOperationException("Unimplemented method 'getChar'");
+        return(SimpleStringBuffer.charAt(i));
     }
 
+    /**
+     * Converts a stringbuffer to a string and returns it.
+     * @return The string version of the stringbuffer.
+     */
     @Override
     public String toString() {
-        throw new UnsupportedOperationException("Unimplemented method 'toString'");
+       return(SimpleStringBuffer.toString());
     }
 }
--- src/test/java/edu/grinnell/csc207/texteditor/SimpleStringBufferTests.java
+++ src/test/java/edu/grinnell/csc207/texteditor/SimpleStringBufferTests.java
@@ -1,5 +1,69 @@
 package edu.grinnell.csc207.texteditor;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+import net.jqwik.api.*;
+import net.jqwik.api.constraints.*;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+    
+import edu.grinnell.csc207.texteditor.SimpleStringBuffer;
+
+
 public class SimpleStringBufferTests {
-    /** TODO: fill me in with unit and property tests! */
+
+    @Test
+    public void Test() {
+        SimpleStringBuffer strBuff = new SimpleStringBuffer();
+        //Insertion check
+        strBuff.insert('a');
+        assertEquals(1, strBuff.getSize());
+        //Deletion check
+        strBuff.insert('b');
+        strBuff.delete();
+        assertEquals(1, strBuff.getSize());
+        //Get cursor position
+        assertEquals(1, strBuff.getCursorPosition());
+        strBuff.insert('c');
+        //Move left
+        strBuff.moveLeft();
+        assertEquals(1,strBuff.getCursorPosition());
+        //Move right
+        strBuff.moveRight();
+        assertEquals(2,strBuff.getCursorPosition());
+        //getChar
+       assertEquals('c',strBuff.getChar(1));
+        String str = strBuff.toString();
+        assertEquals("ac", str);
+        
+        //Corner cases:
+        strBuff.delete();
+        strBuff.delete();
+        assertEquals(0, strBuff.getCursorPosition());
+        
+        strBuff.moveLeft(); //should just return
+       
+        strBuff.moveRight();
+        strBuff.moveRight();
+        strBuff.moveRight(); //should just return
+        
+    }
+    
+    @Property
+    public SimpleStringBuffer greaterThanZero (@ForAll @IntRange(min=1,max=100) int val) {
+       SimpleStringBuffer strBuff = new SimpleStringBuffer();
+       strBuff.insert('b');
+       strBuff.insert('b');
+       strBuff.insert('b');
+       strBuff.insert('b');
+       strBuff.insert('b');
+       strBuff.insert('b');
+       for (int i = 0; i < val; i++) {
+          strBuff.delete(); 
+       }
+       return strBuff;
+    }
+
 }
